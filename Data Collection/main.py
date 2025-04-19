9d22adff7ec3e133e93f9f043fd30e305a47bf5d
unknown <arijitkarmakar056@gmail.com>
2025-04-19 22:23:41 +0530

    update

--- Data Collection/main.py
+++ Data Collection/main.py
@@ -4,13 +4,23 @@ from ET_get_news_data_modified import scrape_news
 from multiprocessing import cpu_count
 
 if __name__ == "__main__":
-    # getDataForYear(
-    #     year=2023,
-    #     startMonthIndex=1,
-    #     endMonthIndex=12,
-    #     index = 44927
-    # )
+    index = 39448
+    start_year = 2008
+    end_year = 2022
+    
+    for year in range(start_year, end_year + 1):
+        getDataForYear(
+            year = year,
+            startMonthIndex=1,
+            endMonthIndex=12,
+            index=index
+        )
+        
+        if(year % 4 == 0):
+            index = index + 366
+        else:
+            index = index + 365
 
     # getNewsFromLink()
-    print(f"CPU COUNT = {cpu_count()}")
-    scrape_news()
+    # print(f"CPU COUNT = {cpu_count()}")
+    # scrape_news()
